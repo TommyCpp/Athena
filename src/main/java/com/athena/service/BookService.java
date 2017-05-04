1e589af3242115c76d52cf910e6525d9dfaea035
吴钟扬 <zhongyang.wu@outlook.com>
2017-05-04 10:31:36 +0800

    测试了关联关系

--- src/main/java/com/athena/service/BookService.java
+++ src/main/java/com/athena/service/BookService.java
@@ -1,12 +1,15 @@
 package com.athena.service;
 
+import com.athena.model.Book;
 import com.athena.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tommy on 2017/3/28.
- *
  */
 @Service
 public class BookService {
@@ -17,5 +20,15 @@ public class BookService {
     @Autowired
     public BookService(BookRepository repository) {
         this.repository = repository;
+    }
+
+
+    public List<Book> searchBookByName(String[] names) {
+        List<Book> result = new ArrayList<>();
+        for (String name :
+                names) {
+            result.addAll(this.repository.getBooksByTitleExists(name));
+        }
+        return result;
     }
 }
