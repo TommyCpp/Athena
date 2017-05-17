c94e7fa0ea2a3f41cfc030c101e0ea3272faf7f1
TommyCpp <a444529216@hotmail.com>
2017-05-17 10:22:03 +0800

    Athena-1 根据书名搜索

--- src/main/java/com/athena/service/PageableHeaderService.java
+++ src/main/java/com/athena/service/PageableHeaderService.java
@@ -15,10 +15,18 @@ import java.util.Map;
 @Service
 public class PageableHeaderService {
 
+    /**
+     * Sets header.
+     *
+     * @param page     the page
+     * @param request  the request
+     * @param response the response
+     * @throws MissingServletRequestPartException the missing servlet request part exception
+     */
     public void setHeader(Page page, HttpServletRequest request, HttpServletResponse response) throws MissingServletRequestPartException {
         response.setHeader("X-Total-Count", Long.toString(page.getTotalElements()));
         String url = "";
-        if(request.getQueryString() == null){
+        if (request.getQueryString() == null) {
             throw new MissingServletRequestPartException("search term");
         }
         if (request.getQueryString().contains("page") || request.getQueryString().contains("last_cursor")) {
@@ -28,12 +36,33 @@ public class PageableHeaderService {
         response.setHeader("Links", "");
     }
 
-    public Map<String,String> getQueryMap(String queryString){
+    private Map<String, String> getQueryMap(String queryString) {
         Map<String, String> result = new HashMap<>();
         String[] keyValues = queryString.split("&");
-        for(String keyValue:keyValues){
+        for (String keyValue : keyValues) {
             int divider = keyValue.indexOf("=");
-            result.put(keyValue.substring(0, divider), keyValue.substring(divider));
+            String key = keyValue.substring(0, divider);
+            String value = keyValue.substring(divider + 1);
+            if (result.containsKey(key)) {
+                result.put(key, result.get(key) + "," + value);
+            } else {
+                result.put(key, value);
+            }
+        }
+        return result;
+    }
+
+    private String getQuery(Map<String, String> map) {
+        String result = "";
+        boolean isFirst = true;
+        for (String key :
+                map.keySet()) {
+            if (isFirst) {
+                result += key + "=" + map.get(key);
+                isFirst = false;
+            } else {
+                result += "&" + key + "=" + map.get(key);
+            }
         }
         return result;
     }
--- src/test/java/com/athena/service/PageableHeaderServiceTest.java
+++ src/test/java/com/athena/service/PageableHeaderServiceTest.java
@@ -1,10 +1,18 @@
 package com.athena.service;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by tommy on 2017/5/16.
@@ -12,13 +20,54 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PageableHeaderServiceTest {
-    @Autowired
-    private PageableHeaderService service;
+
+    private Method getQueryMap;
+    private Method getQuery;
+
+    @Before
+    public void setup() {
+        try {
+            this.getQueryMap = PageableHeaderService.class.getDeclaredMethod("getQueryMap", String.class);
+            getQueryMap.setAccessible(true);
+            this.getQuery = PageableHeaderService.class.getDeclaredMethod("getQuery", Map.class);
+            this.getQuery.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void testGetQueryMap() {
         String query = "author=test,test&last_cursor=4&count=20";
-        service.getQueryMap(query);
-//TODO:Continue
+        Map<String, String> result = null;
+        try {
+            result = (Map<String, String>) this.getQueryMap.invoke(PageableHeaderService.class.newInstance(), query);
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        Map<String, String> except = new HashMap<>();
+        except.put("author", "test,test");
+        except.put("last_cursor", "4");
+        except.put("count", "20");
+        Assert.assertTrue(except.equals(result));
     }
+
+    @Test
+    public void testGetQuery() {
+        Map<String, String> map = new HashMap<>();
+        map.put("author", "test,test");
+        map.put("last_cursor", "4");
+        map.put("count", "20");
+
+        String except = "last_cursor=4&author=test,test&count=20";
+        String result = null;
+        try {
+            result = (String) this.getQuery.invoke(PageableHeaderService.class.newInstance(), map);
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(except, result);
+    }
+
 }
