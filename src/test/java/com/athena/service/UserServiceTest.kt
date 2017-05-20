19d9a2ebd8213a66b7d36cb18d2504fd9ad80e69
吴钟扬 <zhongyang.wu@outlook.com>
2017-05-20 23:32:33 +0800

    Athena-24 使用Kotlin重构现有的单元测试

---
+++ src/test/java/com/athena/service/UserServiceTest.kt
@@ -0,0 +1,37 @@
+package com.athena.service
+
+import com.athena.model.User
+import com.github.springtestdbunit.DbUnitTestExecutionListener
+import com.github.springtestdbunit.annotation.DatabaseSetup
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener
+import javax.transaction.Transactional
+
+/**
+ * Created by tommy on 2017/5/20.
+ *
+ */
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@Transactional
+@TestExecutionListeners(DependencyInjectionTestExecutionListener::class, DbUnitTestExecutionListener::class, TransactionalTestExecutionListener::class)
+@DatabaseSetup("classpath:users.xml")
+open class UserServiceTest {
+    @Autowired
+    lateinit var service: UserService
+
+    @Test
+    fun testGetUser() {
+        val user = User()
+        user.username = "test"
+        user.id = 11L
+        Assert.assertEquals(user.username, service.getUser(11L).username)
+    }
+}
