80448b67264c0ed09008ca9369719a157ba6285d
TommyCpp <a444529216@hotmail.com>
2017-08-21 12:22:16 +0800

    Athena-50 获取Batch

--- src/main/java/com/athena/controller/BatchController.java
+++ src/main/java/com/athena/controller/BatchController.java
@@ -3,18 +3,15 @@ package com.athena.controller;
 import com.athena.exception.ResourceNotFoundException;
 import com.athena.model.Batch;
 import com.athena.security.model.Account;
+import com.athena.security.service.AccountService;
 import com.athena.service.BatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
 
 /**
  * Created by Tommy on 2017/8/17.
@@ -23,19 +20,22 @@ import java.util.Collection;
 @RequestMapping("${web.url.prefix}/batch/**")
 public class BatchController {
     private final BatchService batchService;
+    private final AccountService accountService;
 
     @Autowired
-    public BatchController(BatchService batchService) {
+    public BatchController(BatchService batchService, AccountService accountService) {
         this.batchService = batchService;
+        this.accountService = accountService;
     }
 
     @RequestMapping(path = "/{uuid}", method = RequestMethod.GET)
     public ResponseEntity<Batch> batch(@PathVariable String uuid, @AuthenticationPrincipal Account user) throws ResourceNotFoundException {
-        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
-        if (authorities.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
-            // if it is admin
+        if (this.accountService.privilegeBigger(user,"ROLE_READER")) {
+            // if it is admin or above
             return ResponseEntity.ok(this.batchService.findOne(uuid));
+        } else {
+            // if it is a reader or low
+            return ResponseEntity.ok(this.batchService.findOne(uuid, "ROLE_READER"));
         }
-        return ResponseEntity.ok(this.batchService.findOne(uuid, "ROLE_READER"));
     }
 }
