68f2d41a6cd8307582232539908284cebbb9e103
TommyCpp <a444529216@hotmail.com>
2017-11-23 23:54:16 +0800

    Athena-82 DamageHandlerService

---
+++ src/main/java/com/athena/service/SystemMessageService.java
@@ -0,0 +1,15 @@
+package com.athena.service;
+
+import com.athena.model.AbstractMessage;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Tommy on 2017/11/22.
+ */
+@Service
+public class SystemMessageService implements MessageService{
+    @Override
+    public void send(AbstractMessage message) {
+
+    }
+}
