e43e8e6fd5c758b4916f8f02422c73a8158b8d36
Zhongyang.Wu <zhongyang.wu@outlook.com>
2018-01-04 16:33:33 +0800

    Add HomePage and router

--- frontend/web/src/app/app.module.ts
+++ frontend/web/src/app/app.module.ts
@@ -6,19 +6,25 @@ import {AppComponent} from './app.component';
 import {NavbarComponent} from './shared/navbar/navbar.component';
 import {MatButtonModule, MatMenuModule, MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Router, RouterModule} from '@angular/router';
+import {ATHENA_ROUTES} from './routes';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ATHENA_ROUTES),
+    Router
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/web/src/app/routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+
+export const ATHENA_ROUTES: Routes = [
+  {path: '', component: HomePageComponent}
+];
