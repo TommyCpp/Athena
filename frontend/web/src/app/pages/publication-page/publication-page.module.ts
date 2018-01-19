086485a8d73f6467881e6bdee99292f6d119e461
Zhongyang.Wu <zhongyang.wu@outlook.com>
2018-01-19 16:58:47 +0800

    Search Card

--- frontend/web/src/app/pages/publication-page/publication-page.module.ts
+++ frontend/web/src/app/pages/publication-page/publication-page.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicationPageComponent } from './publication-page.component';
 import { SearchCardComponent } from './search-card/search-card.component';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   declarations: [PublicationPageComponent, SearchCardComponent]
 })
--- frontend/web/src/app/pages/publication-page/search-card/search-card.component.ts
+++ frontend/web/src/app/pages/publication-page/search-card/search-card.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-card.component.scss']
 })
 export class SearchCardComponent implements OnInit {
+  searchValue: string;
 
   constructor() { }
 
