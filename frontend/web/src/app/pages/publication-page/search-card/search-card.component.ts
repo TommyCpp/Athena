06df86f4aff2fba18b493852807685dc20d90917
TommyCpp <a444529216@hotmail.com>
2018-02-27 23:28:44 +0800

    SearchCard

--- frontend/web/src/app/pages/publication-page/search-card/search-card.component.ts
+++ frontend/web/src/app/pages/publication-page/search-card/search-card.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CardComponent} from '../../../shared/card/card.component';
 
 @Component({
   selector: 'app-search-card',
   templateUrl: './search-card.component.html',
   styleUrls: ['./search-card.component.scss']
 })
-export class SearchCardComponent implements OnInit {
+export class SearchCardComponent extends CardComponent implements OnInit {
   searchValue: string;
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
